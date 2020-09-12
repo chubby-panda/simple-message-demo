7d60d51bc8e255efd23e8214ab64377f77318828
Sophie Phoon <sophie.j.phoon@gmail.com>
2020-09-12 10:29:51 +0800

    button added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Change <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // variables
+    // const message = 'JavaScript is cool!'
+    const [message, setMessage] = React.useState('JavaScript is cool!');
+
+    function handleClick() {
+        setMessage('Or is it?')
+    }
+
+
+    // template
+    return (
+    <div>
+        <h1>{message}</h1>
+        <button onClick={handleClick}>Update the message!</button>
     </div>
-  );
+    )
 }
 
 export default App;
